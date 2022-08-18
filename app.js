e8b92fcd8370447542520f94c3f69e27a90cffad
Duoc Dev <78975249+duocdev@users.noreply.github.com>
2022-08-18 23:44:05 +0700

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const PORT = process.env.PORT || 3000
 const express = require('express')
+const cors = require('cors')
 const app = express();
+app.use(cors())
 app.use('/',express.static('public'))
 app.listen(PORT, () => { console.log("server is running...") })
